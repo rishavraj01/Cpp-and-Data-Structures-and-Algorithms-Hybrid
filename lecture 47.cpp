0f5d867240af04f0ce801a669543ca2030954fdb
Rishav Raj <120318036+rishavraj01@users.noreply.github.com>
2024-07-17 08:47:08 +0530

    lecture 47.cpp

---
+++ lecture 47.cpp
@@ -0,0 +1,9 @@
+// Greedy Algorithms li Part-1
+#include<iostream>
+using namespace std;
+int main()
+{
+     cout<<"There is not any coding part in this lecture ";
+     int start[9];
+     int end[9];     
+}
