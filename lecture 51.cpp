f059b3e41e4586e64d0bf80a8bb64c5c03084407
Rishav Raj <120318036+rishavraj01@users.noreply.github.com>
2024-07-18 07:20:27 +0530

    lecture 51.cpp

---
+++ lecture 51.cpp
@@ -0,0 +1,34 @@
+// Introduction To Classes Pt-2
+#include <iostream>
+using namespace std;
+
+class student
+{
+    public:
+
+    string name;
+    int mids;
+    float ends;
+    bool pre_abs;
+
+}; 
+
+class vehicle
+{
+    public:
+
+    string car_name;
+    string tyre_name;
+    int number_of_tyres;
+int Number_of_seats;
+
+};
+
+int main()
+{
+    
+   student a,b,c;
+   int d,e,f;
+   vehicle hyundai, suzuki,ferrari
+
+}
