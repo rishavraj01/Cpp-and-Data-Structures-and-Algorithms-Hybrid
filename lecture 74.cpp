1b987bdaa31c931ddc237e200835d5662a55bf09
Rishav Raj <120318036+rishavraj01@users.noreply.github.com>
2024-07-18 09:18:39 +0530

    lecture 74.cpp

---
+++ lecture 74.cpp
@@ -0,0 +1,10 @@
+// Course Recap 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"This is a recap lecture video in this video we discuss what we had learned in the course . ";
+    cout<<"IN this video there is no coding part ";
+    
+}
