bbe2cd23ca18a2c88952154d7fafc7215a86af19
Rishav Raj <120318036+rishavraj01@users.noreply.github.com>
2024-07-18 09:00:50 +0530

    lecture 68.cpp

---
+++ lecture 68.cpp
@@ -0,0 +1,22 @@
+// Trees Pt-1
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+
+    int data;
+    Node * left;
+    Node * right;
+
+
+};
+
+int main()
+{
+    string CEO;
+
+    string PA,CFO;
+
+}
